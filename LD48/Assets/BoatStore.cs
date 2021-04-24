bd50396da09ec3033941f4221ab980170a46a0e6
christophe-roberge-knowledge <60977802+christophe-roberge-knowledge@users.noreply.github.com>
2021-04-24 15:59:21 -0400

    Inventory 1.1

---
+++ LD48/Assets/BoatStore.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatStore : MonoBehaviour
+{
+    [SerializeField] private GameObject textSellInventory;
+
+    private float inventoryReward;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textSellInventory.SetActive(false);
+        inventoryReward = 0;
+    }
+
+    
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            textSellInventory.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                sellInventory();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            textSellInventory.SetActive(false);
+        }
+    }
+
+    private void sellInventory()
+    {
+        print("Sold inventory!");
+        //Boucle sur l'inventaire
+        for (int i = 0; i < CollisionManager.inventory.Count; i++)
+        {
+            
+        }
+
+    }
+}
--- LD48/Assets/MapGeneration/MapGenerator.cs
+++ LD48/Assets/MapGeneration/MapGenerator.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using Parabox.CSG;
 using System.Linq;
 
 namespace Assets.MapGeneration
