427ac776b35e6590b8fbd3acf285cfad96c1dc50
MrBamboo <nicolas@bamboosoft.ca>
2021-04-24 21:20:43 -0400

    feat: Create deepness color management

---
+++ LD48/Assets/SubmarineManagement/Submarine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Submarine
+{
+    public interface ISubmarine
+    {
+        int Deepness { get; }
+    }
+
+    public class Submarine : MonoBehaviour, ISubmarine
+    {
+        public static ISubmarine Instance { get; private set; }
+
+        public int Deepness => (int)(-Mathf.Clamp(transform.position.y, float.MinValue, 0));
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+    }
+}
